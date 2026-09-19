8c4f6eabfa8ddd2e5d5201f22fae27f9324ee73b
BIMAN SINGHA <singhabiman33@gmail.com>
2026-09-19 22:07:28 +0530

    Update Resend sender email

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -23,7 +23,7 @@ export const submitContactForm = async (req, res) => {
 
         // Send notification email
         const { data, error } = await resend.emails.send({
-            from: 'Portfolio <onboarding@resend.dev>',
+            from: 'Portfolio <[EMAIL_ADDRESS]>',
             to: [process.env.EMAIL_USER],
             subject: `New Portfolio Message from ${name}`,
             replyTo: email,
